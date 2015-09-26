ef9f6b553a304754cc1323bab1bdb35d1cdd1acf
Raju <rajuru@gmail.com>
2015-09-26 02:15:09 -0400

    Add thread for faster download

--- download.rb
+++ download.rb
@@ -1,49 +1,67 @@
 #!/usr/bin/env ruby
 require 'mechanize'
 require 'fileutils'
+require 'logger'
+
 
 url = ARGV[0]
 base_directory = ARGV[1] || nil
 
 @base_directory = base_directory || './'
 @agent = Mechanize.new
+@agent.user_agent_alias = 'Mac Safari'
+@agent.log = Logger.new "scrap.log"
+
 
 def get_title(title)
-  title.split(' - ')[1].to_s.sub(' (music.com.bd)','')
+  title.split('/').last.to_s.sub(' (music.com.bd)', '')
 end
 
 def get_directory(title)
   title.split(' - ')[0]
 end
 
-
-def save_music(title, link)
+def save_music(link)
   unless !!link
     return false
   end
-  directory = get_directory(title)
-  file = get_title(title)
+  directory = @page_title
+  file = get_title(link)
 
-  music = @agent.get(link)
+  unless file
+    return
+  end
 
   full_dir = "#{@base_directory}/#{directory}"
+
   FileUtils.mkdir_p(full_dir)
-  File.open("#{full_dir}/#{file}", 'wb'){ |f| f << music.body }
+
+  music = @agent.get(link)
+  File.open("#{full_dir}/#{file}", 'wb') { |f| f << music.body }
+end
+
+def parse_page(page_link)
+  content = @agent.get(page_link)
+  music_link = content.search("a.btn-dl")[0]['href'].to_s.strip
+
+  puts "Saving #{music_link}"
+  save_music(music_link)
 end
 
 page = @agent.get(url)
 
+@page_title = page.title.to_s.split(' > ')[2]
 music_links = page.links_with(class: 'list-group-item')
+
+threads = []
 music_links.each_with_index do |link, idx|
-  if idx == 0
-    next #it's a link to previous page
+  unless link.href.to_s.end_with? '.mp3.html'
+    next
   end
-  content = @agent.get(link.href)
-  music_link = content.search("a.btn-dl")[0]['href'].to_s.strip
 
-  title = music_link.split('/').last
-
-  puts "Saving #{music_link}"
-  save_music(title, music_link)
-
+  threads << Thread.new do
+    parse_page(link.href)
+  end
 end
+
+threads.each(&:join)
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
+# HEADS UP, this is a one off build. Code isn't clean and may not be even improved!
+
 # Installations
 
 Run `bundle`
 
 
-#Usages
+# Usages
 
 - `chmod +x download.rb`
 - ./download http://www.music.com.bd/download/browse/P/Polli%20Geeti/ /Music
@@ -11,3 +13,5 @@ Run `bundle`
 It will download Polli Geeti album into Music directory. It'll create a directory namd 'Polli Geeti'.
 
 
+# Caution
+It uses a new thread for downloading each link. There is no thread pool. So be careful to use with page where it has too many files to download
